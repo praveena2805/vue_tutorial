928c9eb7f6fc6e11fc953fb9f1e7933770e69485
triupura drupal8 <triupuradrupal@gmail.com>
2020-04-11 15:37:23 +0530

    first lesson

---
+++ new.js
@@ -0,0 +1,21 @@
+var app=new Vue({ 
+    el :'#newapp',
+    data:{
+        
+        title:"hello",
+        items:[
+            { text:'hello' },
+            { text:'test' }
+        ],
+    object:{
+        
+        title:"my first lesson",
+        author:"praveena",
+        month:"april"
+    }
+    
+        
+    },
+    
+    
+}) 
